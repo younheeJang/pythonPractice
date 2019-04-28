5855b53aa3578641097d88f80ef777020e82ba76
younheeJang <edam7635@gmail.com>
2019-04-28 22:49:12 +0900

    get all in new folder

---
+++ python_basical_practice/is_evenly_number_ornot.py
@@ -0,0 +1,10 @@
+def is_evenly_divisible(number):
+    if (number % 2 == 0):
+        return True
+    else:
+        return False
+
+
+print(is_evenly_divisible(3))
+print(is_evenly_divisible(7))
+print(is_evenly_divisible(8))
---
+++ python_basical_practice/practice_while.py
@@ -0,0 +1,10 @@
+i = 1
+while i <= 100:
+    if(i%2==0):
+    	print(i)
+    i = i + 1
+
+i = 100
+while i % 23 != 0:
+	i = i + 15
+print(i)
