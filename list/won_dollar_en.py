2646f09f956063a9b9fbb2d47d628a2c135f3fe0
윤희 장 <nonefornone@bitbucket.org>
2019-05-01 16:47:13 +0900

    won dollar en

--- list/won_dollar_en.py
+++ list/won_dollar_en.py
@@ -1,6 +1,5 @@
 # won -> dollar
 def krw_to_usd(won):
-    # 코드를 입력하세요.
     i = 0
     while i < len(won):
         won[i] = won[i] / 1000
@@ -10,13 +9,12 @@ def krw_to_usd(won):
 
 # dollars -> en
 def usd_to_jpy(dollars):
-    # 코드를 입력하세요.
     i = 0
     while i < len(dollars):
         dollars[i] = (dollars[i] / 8) * 1000000
         i = i + 1
     return dollars
-
+ 
 
 #
 amounts = [1000, 2000, 3000, 5000, 8000, 13000, 21000, 34000]
