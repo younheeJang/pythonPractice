ada4423652ee1284a04cdf969314d290accddfb8
윤희 장 <nonefornone@bitbucket.org>
2019-05-10 15:48:41 +0900

    BFS

---
+++ algorithm/BFS.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+# 지하철역 클래스
+class Station:
+    # 코드를 입력하세요.
+    pass
+
+# Breadth-First Search 알고리즘
+def bfs(start, goal):
+    # 코드를 입력하세요.
+    return False
+
+# 파일 읽기
+stations = {}
+in_file = open('data/stations.txt',  encoding='utf-8')
+
+# 코드를 입력하세요
+for line in in_file:
+    print(line.strip())
+    #print(line.strip().split(' - '))
+
+
+in_file.close()
